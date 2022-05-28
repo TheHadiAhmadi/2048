b8d10c0045f90d87513fb003cd4b3419937df85e
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-05-29 00:37:03 +0430

    enable service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,36 +1,36 @@
-// import {build, files, prerendered, version} from '$service-worker'
+import {build, files, prerendered, version} from '$service-worker'
 
-// function installServiceWorker() {
-//     return caches.open('cache-' + version).then((cache) => {
-//         return cache.addAll([...files, ...build, ...prerendered]);
-//     }).then(() => { return self.skipWaiting()}).catch(err => console.log("err: ", err))
-// }
+function installServiceWorker() {
+    return caches.open('cache-' + version).then((cache) => {
+        return cache.addAll([...files, ...build, ...prerendered]);
+    }).then(() => { return self.skipWaiting()}).catch(err => console.log("err: ", err))
+}
 
-// self.addEventListener('activate', (event) => {
-//     console.log('[ServiceWorker] Activate');
-//     // Remove previous cached data from disk
-//     event.waitUntil(
-//       caches.keys().then(async (keys) =>
-//         Promise.all(
-//           keys.map((key) => {
-//             if (key !== 'cache-' + version) {
-//               console.log('[ServiceWorker] Removing old cache', key);
-//               return caches.delete(key);
-//             }
-//           }),
-//         ),
-//       ),
-//     );
-//     self.clients.claim();
-//   });
+self.addEventListener('activate', (event) => {
+    console.log('[ServiceWorker] Activate');
+    // Remove previous cached data from disk
+    event.waitUntil(
+      caches.keys().then(async (keys) =>
+        Promise.all(
+          keys.map((key) => {
+            if (key !== 'cache-' + version) {
+              console.log('[ServiceWorker] Removing old cache', key);
+              return caches.delete(key);
+            }
+          }),
+        ),
+      ),
+    );
+    self.clients.claim();
+  });
 
-// self.addEventListener('install', installServiceWorker);
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.open('cache-' + version).then(cache => {
-//             return cache.match(event.request).then(response => {
-//                 return response
-//             })
-//         })
-//     )
-// });
+self.addEventListener('install', installServiceWorker);
+
+self.addEventListener('activate', e => self.clients.claim());
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+      caches.match(e.request)
+      .then(response => response ? response : fetch(e.request))
+    )
+  });
